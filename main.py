59e6bde311a02a914cfa4ae251d2486e9a09d87c
thepetk <thepetk@gmail.com>
2025-01-14 23:21:51 +0000

    Implement fitness function

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ JOBS_DATA = [
 NUM_MACHINES = 3
 POPULATION_LIMIT = 1000
 
+RAW_TASK = list[tuple[int, int]]
+
 
 @dataclass
 class Chromosome:
@@ -17,35 +19,66 @@ class Chromosome:
 
 
 @dataclass
+class Task:
+    task_id: "int"
+    runtime: "int"
+    machine_id: "int"
+
+
 class Job:
-    data: "list[tuple[int, int]]"
+    def __init__(self, job_id: "int", raw_task_list: "list[RAW_TASK]") -> "None":
+
+        self.job_id = job_id
+        self.tasks = self._init_tasks(raw_task_list)
+        self.tasks_iterator = iter(self.tasks)
+        self.task_end_time: "int" = 0
+
+    def _init_tasks(self, raw_task_list: "list[RAW_TASK]") -> "list[Task]":
+        return [
+            Task(task_id=task_id, runtime=runtime, machine_id=machine_id)
+            for task_id, (machine_id, runtime) in enumerate(raw_task_list)
+        ]
+
+    def reset_tasks(self) -> "None":
+        self.tasks_iterator = iter(self.tasks)
+        self.task_end_time = 0
+
+    def get_next_task(self) -> "Task":
+        return next(self.tasks_iterator)
 
 
 @dataclass
 class Machine:
-    name: "str"
+    machine_id: "int"
+    end_time: "int" = 0
+
+    def reset(self) -> "None":
+        self.end_time = 0
 
 
 class JobScheduler:
 
-    def __init__(
-        self, data: "list[list[tuple[int, int]]]", num_machines: "int"
-    ) -> "None":
+    def __init__(self, data: "list[RAW_TASK]", num_machines: "int") -> "None":
         self.jobs = self._generate_jobs(data)
         self.machines = self._generate_machines(num_machines)
         self.base_chromosome = self._generate_base_chromosome()
 
     def _generate_jobs(self, data: "list[list[tuple[int, int]]]") -> "list[Job]":
-        return [Job(data=row) for row in data]
+        return [Job(job_id=idx, raw_task_list=row) for idx, row in enumerate(data)]
 
     def _generate_machines(self, num_machines: "int") -> "list[Machine]":
-        return [Machine(name=f"m{idx}") for idx in range(1, num_machines)]
+        return [Machine(machine_id=idx) for idx in range(num_machines)]
 
     def _generate_base_chromosome(self) -> "Chromosome":
         _initial_chromosome = [
-            [idx] * len(job.data) for idx, job in enumerate(self.jobs)
+            [idx] * len(job.tasks) for idx, job in enumerate(self.jobs)
         ]
         return Chromosome(data=[l for ll in _initial_chromosome for l in ll])
+
+    def random_chromosome(self) -> "Chromosome":
+        c = copy.deepcopy(self.base_chromosome)
+        random.shuffle(c.data)
+        return c
 
     def create_population(self) -> "list[Chromosome]":
         population: "list[Chromosome]" = []
@@ -61,21 +94,48 @@ class JobScheduler:
         for chromosome in population:
             print(chromosome)
 
-    def random_chromosome(self) -> "Chromosome":
-        c = copy.deepcopy(self.base_chromosome)
-        random.shuffle(c.data)
-        return c
+    def reset(self) -> "None":
+        for job in self.jobs:
+            job.reset_tasks()
+
+        for machine in self.machines:
+            machine.reset()
 
     def validate(self, chromosome: "Chromosome") -> "bool":
         for idx in range(len(self.jobs)):
             job_times = chromosome.data.count(idx)
-            if job_times != len(self.jobs[idx].data):
+            if job_times != len(self.jobs[idx].tasks):
                 print(f"Invalid generated chromosome: {chromosome.data}")
                 return False
         return True
 
-    def fitness(self, chromosome: "Chromosome"):
-        pass
+    def fitness(self, chromosome: "Chromosome") -> "int":
+        self.reset()
+
+        if not self.validate(chromosome):
+            return -1
+
+        for job_idx in chromosome.data:
+            current_job = self.jobs[job_idx]
+            current_task = current_job.get_next_task()
+            machine_id = current_task.machine_id
+            machine = self.machines[machine_id]
+            machine_end_time = machine.end_time
+            job_task_end_time = current_job.task_end_time
+
+            task_start_time = max(machine_end_time, job_task_end_time)
+            task_end_time = task_start_time + current_task.runtime
+
+            machine.end_time = task_end_time
+            current_job.task_end_time = task_end_time
+
+        end_time_max = -1
+        for machine in self.machines:
+            if machine.end_time > end_time_max:
+                end_time_max = machine.end_time
+
+        print("Fitness {} for chromosome {}".format(end_time_max, chromosome))
+        return end_time_max
 
 
 if __name__ == "__main__":
@@ -84,3 +144,4 @@ if __name__ == "__main__":
     scheduler.validate(r)
     population = scheduler.create_population()
     scheduler.show_population(population)
+    scheduler.fitness(r)
